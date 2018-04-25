a629973e7f8308b291c43be05ce07db3ade5d26b
Erin Furuichi <erinf@Erins-MacBook-Pro.local>
2018-04-24 19:04:00 -1000

    1 nav menu

--- js/app.js
+++ js/app.js
@@ -4,7 +4,24 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+var navElem = document.getElementsByClassName("navi");
 
+for(var i = 0; i<navElem.length; i++){
+ 
+  navElem[i].addEventListener("click", showInner);
+
+  }
+
+function showInner(){
+  
+  var innerElem = this.querySelectorAll('.inner')[0];
+  if(innerElem.style.display === 'block'){
+    innerElem.style.display = 'none';
+  }else{
+    innerElem.style.display = 'block';
+  }
+
+}
 
 
 
